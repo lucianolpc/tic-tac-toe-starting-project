45b2819e4078430493777f2d39138dd069970f97
lucianolpc <luciano.junior@dcomp.ufs.br>
2025-08-12 21:50:45 -0300

    Criação da barra de nomes dos jogadores e do tabuleiro do jogo da velha.

---
+++ src/components/Player.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    // console.log(event);
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editablePlayerName = <span>
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={handleChange}
+      />
+    </span>;
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>
+        {isEditing ? "Save" : "Edit"}
+      </button>
+    </li>
+  );
+}
